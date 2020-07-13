d970013b9e84d8b631abfd70f910435e7fb1b3bf
Jason Brower <jtbrower@oceansidesw.com>
2020-07-13 17:20:16 -0500

    Put IMainWindow using's inside namespace

--- src/Windows/WinUI.CustomControls/IMainWindow.cs
+++ src/Windows/WinUI.CustomControls/IMainWindow.cs
@@ -19,10 +19,10 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-using Microsoft.UI.Xaml;
-
 namespace WinUI.CustomControls
 {
+    using Microsoft.UI.Xaml;
+
     ////////////////////////////////////////////////////////////////////////////////////////////////////
     /// <summary>   Interface for main window. </summary>
     ////////////////////////////////////////////////////////////////////////////////////////////////////
